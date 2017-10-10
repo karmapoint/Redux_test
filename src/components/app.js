7da9e45163446932dd9cbe9844ebb7a425bb1b1e
Brent <karmapoint@gmail.com>
2017-10-10 16:46:09 -0700

    add book detail container and actions

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from "../containers/bookList";
+import BookDeatil from "../containers/bookDetail";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDeatil />
       </div>
     );
   }
--- src/containers/bookList.js
+++ src/containers/bookList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect }from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return(
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li key={book.title}
+          onClick={()=> this.props.selectBook(book)}
+          className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -27,5 +31,13 @@ function mapStateToProps(state) {
   };
 }
 
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, the result should be passed to all
+  // reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
 // connect takes a function and a component and returns a container
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
